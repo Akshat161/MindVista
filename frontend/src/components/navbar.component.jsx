ba4c62204cfee96768b76ceef898791f8cc79318
akshat161 <akshat.trivedi1604@gmail.com>
2025-02-10 00:53:48 +0530

    updated logo

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -1,129 +1,131 @@
 
-import { Link ,Outlet, useNavigate} from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import logo from '../imgs/logomv1.png'
 import { useContext, useEffect, useState } from 'react'
-import {UserContext} from '../App.jsx'
+import { UserContext } from '../App.jsx'
 import UserNavigationPanel from './user-navigation.component.jsx'
 import axios from 'axios'
 
 const Navbar = () => {
 
-    const [searchBoxVisibility,setSearchBoxVisibility]=useState(false)
-    const[userNavPanel,setUserNavPanel] =useState(false);
+    const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
+    const [userNavPanel, setUserNavPanel] = useState(false);
 
     let navigate = useNavigate();
 
-    const { userAuth, userAuth: { access_token,profile_img,new_notification_available },setUserAuth} = useContext(UserContext)
+    const { userAuth, userAuth: { access_token, profile_img, new_notification_available }, setUserAuth } = useContext(UserContext)
 
-    useEffect(()=>{
-     if(access_token){
-        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/new-notification",{
-            headers:{
-                "Authorization":`Bearer ${access_token}`
-            }
-        })
-        .then(({ data })=>{
-           setUserAuth({ ...userAuth, ...data })
-        })
-        .catch(err=>{
-            console.log(err);
-        })
-     }
-    },[access_token])
+    useEffect(() => {
+        if (access_token) {
+            axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/new-notification", {
+                headers: {
+                    "Authorization": `Bearer ${access_token}`
+                }
+            })
+                .then(({ data }) => {
+                    setUserAuth({ ...userAuth, ...data })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }, [access_token])
 
-    const handelUserNavPanel= () => {
-        setUserNavPanel(currentVal=>!currentVal);
+    const handelUserNavPanel = () => {
+        setUserNavPanel(currentVal => !currentVal);
     }
 
-    const handleSearch =(e)=>{
-        let query =e.target.value;
+    const handleSearch = (e) => {
+        let query = e.target.value;
 
-        if(e.keyCode==13 && query.length){
+        if (e.keyCode == 13 && query.length) {
             navigate(`/search/${query}`)
         }
     }
 
-    const handelBlur =()=>{
-        setTimeout (()=>{
+    const handelBlur = () => {
+        setTimeout(() => {
             setUserNavPanel(false);
-        },200);
-        
+        }, 200);
+
     }
-    
+
     return (
         <>
-        <nav className="navbar z-50">
+            <nav className="navbar z-50">
+                <div className="flex items-center ">
+                    <Link to="/" className="flex-none w-15 h-20">
+                        <img src={logo} className="w-full ml-0" />
+                    </Link>
+                    <p className="text-2xl md:text-lg lg:text-3xl font-bold">MindVista</p>
+                </div>
 
-            <Link to="/" className="flex-none w-15 h-20  ">
-                <img src={logo} className="w-full ml-0" />
-            </Link>
+                <div className={'absolute bg-white w-full left-0 top-full mt-0.! border-b border-grey md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto py-4 px-[5vw] md:show ' + (searchBoxVisibility ? "show" : "hide")}>
+                    <input
+                        type='text'
+                        placeholder='Search'
+                        className='w-full md:w-auto bg-grey p-4 pl-6 pr-[12% md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12'
+                        onKeyDown={handleSearch}
+                    />
 
-            <div className={'absolute bg-white w-full left-0 top-full mt-0.! border-b border-grey md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto py-4 px-[5vw] md:show ' + (searchBoxVisibility?"show":"hide")}>
-                <input
-                    type='text'
-                    placeholder='Search'
-                    className='w-full md:w-auto bg-grey p-4 pl-6 pr-[12% md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12'
-                    onKeyDown={handleSearch}
-                />
+                    <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
 
-                <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
-      
-            </div>
+                </div>
 
-            <div className='flex items-center gap-3 md:gap-6 ml-auto'>
+                <div className='flex items-center gap-3 md:gap-6 ml-auto'>
 
-               <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center ' 
-                       onClick={()=>setSearchBoxVisibility(currentVal=> !currentVal)}>
+                    <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center '
+                        onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}>
 
-                <i className="fi fi-rr-search text-xl" ></i>
+                        <i className="fi fi-rr-search text-xl" ></i>
 
-               </button>
+                    </button>
 
-               <Link to="/editor" className='hidden md:flex gap-2 link'>
-                <i  className="fi fi-rr-file-edit"></i>
-                <p>Write</p>
-               </Link>
-
-
-               {
-                 access_token ?
-                 <>
-                     <Link to="/dashboard/notifications">
-                        <button className='w-12 h-12 rounded-full bg-grey relative hover:bg-black/10'>
-                            <i className='fi fi-rr-bell text-2xl block mt-1'></i>
-                            {
-                                new_notification_available ? <span className='bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2'></span> :""
-                            }
-                        
-                        </button>
-                     </Link>
-
-                     <div className="relative" onClick={handelUserNavPanel} onBlur={handelBlur}>
-
-                        <button className='w-12 h-12 mt-1'>
-                            <img src={profile_img} className='w-full h-full object-cover rounded-full'  referrerPolicy="no-referrer"/>
-                        </button>
-
-                        {
-                            userNavPanel ? <UserNavigationPanel/> :""
-                        }
-                     </div>
-                 </>
-                 :
-                 <>
-                    <Link to="/signin" className="btn-dark py-2">
-                        Sign In
+                    <Link to="/editor" className='hidden md:flex gap-2 link'>
+                        <i className="fi fi-rr-file-edit"></i>
+                        <p>Write</p>
                     </Link>
 
-                    <Link to="/signup" className="btn-light py-2 hidden md:block">
-                        Sign Up
-                    </Link>
-                 </>
-               }
-            </div>
 
-        </nav>
-        <Outlet/>
+                    {
+                        access_token ?
+                            <>
+                                <Link to="/dashboard/notifications">
+                                    <button className='w-12 h-12 rounded-full bg-grey relative hover:bg-black/10'>
+                                        <i className='fi fi-rr-bell text-2xl block mt-1'></i>
+                                        {
+                                            new_notification_available ? <span className='bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2'></span> : ""
+                                        }
+
+                                    </button>
+                                </Link>
+
+                                <div className="relative" onClick={handelUserNavPanel} onBlur={handelBlur}>
+
+                                    <button className='w-12 h-12 mt-1'>
+                                        <img src={profile_img} className='w-full h-full object-cover rounded-full' referrerPolicy="no-referrer" />
+                                    </button>
+
+                                    {
+                                        userNavPanel ? <UserNavigationPanel /> : ""
+                                    }
+                                </div>
+                            </>
+                            :
+                            <>
+                                <Link to="/signin" className="btn-dark py-2">
+                                    Sign In
+                                </Link>
+
+                                <Link to="/signup" className="btn-light py-2 hidden md:block">
+                                    Sign Up
+                                </Link>
+                            </>
+                    }
+                </div>
+
+            </nav>
+            <Outlet />
         </>
     )
 }
